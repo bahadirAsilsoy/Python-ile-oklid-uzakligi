ffcfd5fc108c3fb64da9a58b5f0c9205ab56e2ba
Bahadır <82715842+bahadirAsilsoy@users.noreply.github.com>
2024-05-23 13:40:05 +0300

    Create oklidUzakligi.py

---
+++ oklidUzakligi.py
@@ -0,0 +1,20 @@
+import math
+
+points = [(2, 3), (5, 7), (1, 9), (4, 6), (8, 2)]
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+min_distance = min(distances)
+
+print("Minimum mesafe:", min_distance)
+
